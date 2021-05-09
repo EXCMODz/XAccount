5a80765861aadc6aed23e9bb40ac06a1a33ac9bd
EXCMODz <83895812+EXCMODz@users.noreply.github.com>
2021-05-09 11:40:21 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+pkg update && pkg upgrade
+pkg install python
+pkg install python2
+pip install mechanize
+pip install request
+pkg install git
+git clone https://github.com/EXCMODz/XAccount
+cd XAccount
+python2 HACKED-ACCOUNT.py
